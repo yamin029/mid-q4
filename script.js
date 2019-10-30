e9cd9b2486e8882c73283670b5fa526ebb09611d
cis101 <cis101@secs.iub.edu.bd>
2019-10-30 09:19:25 +0600

    task 2

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ $('#list-btn').click(function(){
         console.log(response)
     })
 })
+
+$('.btn-add').click(function(){
+    let user = {name:'',email:'',password:''}
+    user.name = $('#input-name').val()
+    user.email = $('#input-email').val()
+    user.password = $('#input-password').val()
+
+    $.ajax({
+        method: 'POST',
+        url: 'http://skeleton.mernbook.com/api/users',
+        data: user
+    }).done(function(response){
+        console.log(response)
+        $('#input-name').val('')
+        $('#input-email').val('')
+        $('#input-password').val('')
+        $('#error').css("color", "blue");
+        $('#error').text(response.message)
+    }).fail(function(response){
+        $('#error').text(response.responseJSON.error)
+    })
+})
