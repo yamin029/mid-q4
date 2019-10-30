c8f390a28d89d8f5387883e1a6112e88ce829859
cis101 <cis101@secs.iub.edu.bd>
2019-10-30 08:52:04 +0600

    task 1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 /* Add your script here */
+$('#list-btn').click(function(){
+    $.ajax({
+        method: 'GET',
+        url: 'http://skeleton.mernbook.com/api/users'
+    }).done(function(response){
+        console.log(response)
+        response.forEach(element => {
+            let user = $('<p></p>').text(element.name)
+            $('#users').append(user)
+        });
+    }).fail(function(response){
+        console.log(response)
+    })
+})
